4ede1ad86aafde1a975de2da0c6b876e986db18b
islam <eslamkhalil851@gmail.com>
2024-03-08 19:17:28 +0200

    fixe syntex error

--- src/components/ImageCard.tsx
+++ src/components/ImageCard.tsx
@@ -5,13 +5,11 @@ import React from 'react';
 
 interface ProductCardProps {
     _id: string;
-    imagePath: String,
-    imageName: String,
+    imagePath: string;
+    imageName: string;
 }
 
 const ProductCard: React.FC<ProductCardProps> = ({ _id, imagePath, imageName }) => {
-
-
     return (
         <div className='min-w-[20rem] max-w-[20rem] h-[20rem] flex flex-col items-center justify-start gap-2 bg-white overflow-hidden rounded truncate'>
             <Image alt={imageName} src={imagePath} width={200} height={200} className='w-[100%] h-[100%] object-cover object-center' />
